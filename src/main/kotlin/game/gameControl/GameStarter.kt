63960442aabd904d8ca7f8b8b7a7babe8b5b82aa
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-05-29 23:01:32 +0300

    Update game starter to return action manager and read only context

--- src/main/kotlin/game/gameControl/GameStarter.kt
+++ src/main/kotlin/game/gameControl/GameStarter.kt
@@ -10,7 +10,7 @@ class GameStarter {
     fun start(
         config: GameConfig,
         seed: Int? = null,
-    ): ActionManager {
+    ): Pair<ActionManager, ReadOnlyGameContext> {
         val humans = HumanFactory().createDefaultShuffledHumans(config.playerCount, config.startHealth, seed)
         val players = PlayerFactory().createPlayers(config.playerNames, humans)
 
@@ -18,8 +18,9 @@ class GameStarter {
         val board = BoardFactory().createDefaultBoard(humans, deck, seed)
 
         val context = GameContext(board, players, config.localization, seed = seed)
+        val readOnlyContext = ReadOnlyGameContext(context)
         val manager = ActionManager(context)
 
-        return manager
+        return manager to readOnlyContext
     }
 }
