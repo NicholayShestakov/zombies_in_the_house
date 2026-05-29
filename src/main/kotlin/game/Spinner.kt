3cba9b134f8460dbd477f875e8d959d10ebe7439
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-05-29 08:09:48 +0300

    Update spinner realization. Add seeded random

--- src/main/kotlin/game/Spinner.kt
+++ src/main/kotlin/game/Spinner.kt
@@ -1,9 +1,11 @@
+package game
+
 import kotlin.random.Random
 
 object Spinner {
-
-    fun getValue(): SpinnerValue {
-        return when (Random.nextInt(0, 10)) {
+    fun getValue(seed: Int? = null): SpinnerValue {
+        val random = seed?.let { Random(seed) } ?: Random
+        return when (random.nextInt(0, 10)) {
             in 0..2 -> SpinnerValue.ESCAPE
             in 3..5 -> SpinnerValue.FANGS
             in 6..7 -> SpinnerValue.BLADES
--- src/main/kotlin/game/SpinnerValue.kt
+++ src/main/kotlin/game/SpinnerValue.kt
@@ -1,6 +1,8 @@
-enum class SpinnerValue(number: Int) {
+package game
+
+enum class SpinnerValue(val number: Int) {
     ESCAPE(1),
     FANGS(2),
     BLADES(3),
-    AIM(4)
+    AIM(4),
 }
