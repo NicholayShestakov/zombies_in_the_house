d40bdf23a36f6e8ba6e93597b48d05ce4cc69dc3
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-05-29 22:54:28 +0300

    Add Compose plugins for GUI

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     kotlin("jvm") version "2.3.10"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    id("org.jetbrains.compose") version "1.7.3"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10"
 }
 
 group = "org.example"
@@ -9,10 +11,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material)
+    implementation(compose.runtime)
+    implementation("org.jetbrains.androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("org.jetbrains.androidx.navigation:navigation-compose:2.8.0-alpha10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.8.1")
 }
 
 kotlin {
